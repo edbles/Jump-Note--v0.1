2caf74292aba3265f8c497fe523cf0ae60834361
edbles <edbles@gmail.com>
2015-04-07 20:39:54 -0400

    Prototype
    Prototype with Basic Note movement

---
+++ Assets/Scripts/PhraseController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhraseController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = new Vector2 (0, 0);
+		rigidbody2D.AddForce (new Vector2 (-500, 0));
+	}
+}
